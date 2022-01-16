54dead98dbd54de8ffb75c84c5a43976a54f0d20
mypc <mypc@DESKTOP-9R21U0Q>
2022-01-16 02:44:44 -0500

    First Commit

---
+++ src/main/java/Slenium/Test/App.java
@@ -0,0 +1,13 @@
+package Slenium.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
